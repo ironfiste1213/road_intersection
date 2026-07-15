f22a42dbeec5cf743d6d4febdbcd0c7702a0687a
ybourazz ybourazz <nite246135@gmail.com>
2026-07-15 14:56:56 +0100

    add car direction change at intersection

--- src/main.rs
+++ src/main.rs
@@ -35,76 +35,147 @@ struct Car {
     color: Color,
     x: f32,
     y: f32,
+    depart: Depart,
     direction: Direction,
+}
+
+struct Lights {
+    x: f32,
+    y: f32,
+    is_on: bool,
 }
 
 #[macroquad::main(window_conf)]
 async fn main() {
 
+    let car_colors = [BLUE, RED, YELLOW];
+    let depart_up_dir = [Direction::East, Direction::North, Direction::West];
+    let depart_down_dir = [Direction::West, Direction::South, Direction::East];
+    let depart_left_dir = [Direction::South, Direction::East, Direction::North];
+    let depart_right_dir = [Direction::North, Direction::West, Direction::South];
     let mut cars = Vec::new();
+    let lights = vec![
+        Lights {
+            x: 285.0,
+            y: 275.0,
+            is_on: false,
+        },
+        Lights {
+            x: 405.0,
+            y: 275.0,
+            is_on: false,
+        },
+        Lights {
+            x: 285.0,
+            y: 405.0,
+            is_on: true,
+        },
+        Lights {
+            x: 405.0,
+            y: 405.0,
+            is_on: false,
+        },
+    ];
 
     loop {
-        clear_background(DARKGRAY);
-
+        clear_background(DARKGREEN);
+        
+        draw_rectangle(0.0, 300.0, 700.0, 100.0, DARKGRAY);
+        draw_rectangle(300.0, 0.0, 100.0, 700.0, DARKGRAY);
         draw_line(300.0, 0.0, 300.0, 700.0, 2.0, LIGHTGRAY);
-        draw_line(350.0, 0.0, 350.0, 700.0, 2.0, LIGHTGRAY);
+        draw_line(350.0, 0.0, 350.0, 700.0, 2.0, ORANGE);
         draw_line(400.0, 0.0, 400.0, 700.0, 2.0, LIGHTGRAY);
         draw_line(0.0, 300.0, 700.0, 300.0, 2.0, LIGHTGRAY);
-        draw_line(0.0, 350.0, 700.0, 350.0, 2.0, LIGHTGRAY);
+        draw_line(0.0, 350.0, 700.0, 350.0, 2.0, ORANGE);
         draw_line(0.0, 400.0, 700.0, 400.0, 2.0, LIGHTGRAY);
+        draw_rectangle(300.0, 300.0, 100.0, 100.0, DARKGRAY);
+        draw_rectangle(299.0, 301.0, 99.0, 101.0, DARKGRAY);
+        draw_rectangle(301.0, 299.0, 101.0, 99.0, DARKGRAY);
 
-         if is_key_pressed(KeyCode::Up) {
+         if is_key_pressed(KeyCode::Down) {
+            let r = macroquad::rand::gen_range(0, 3);
             cars.push(Car {
                 x: 305.0,
                 y: -40.0,
-                direction: Direction::Up,
-                color: RED,
+                depart: Depart::Up,
+                direction: depart_down_dir[r],
+                color: car_colors[r],
             });
         }
 
-        if is_key_pressed(KeyCode::Down) {
+        if is_key_pressed(KeyCode::Up) {
+            let r = macroquad::rand::gen_range(0, 3);
             cars.push(Car {
                 x: 355.0,
                 y: 700.0,
-                direction: Direction::Down,
-                color: BLUE,
-            });
-        }
-
-        if is_key_pressed(KeyCode::Left) {
-            cars.push(Car {
-                x: -40.0,
-                y: 355.0,
-                direction: Direction::Left,
-                color: GREEN,
+                depart: Depart::Down,
+                direction: depart_up_dir[r],
+                color: car_colors[r],
             });
         }
 
         if is_key_pressed(KeyCode::Right) {
+            let r = macroquad::rand::gen_range(0, 3);
+            cars.push(Car {
+                x: -40.0,
+                y: 355.0,
+                depart: Depart::Left,
+                direction: depart_right_dir[r],
+                color: car_colors[r],
+            });
+        }
+
+        if is_key_pressed(KeyCode::Left) {
+            let r = macroquad::rand::gen_range(0, 3);
             cars.push(Car {
                 x: 700.0,
                 y: 305.0,
-                direction: Direction::Right,
-                color: YELLOW,
+                depart: Depart::Right,
+                direction: depart_left_dir[r],
+                color: car_colors[r],
             });
         }
 
         for car in &mut cars {
             
             draw_rectangle(car.x, car.y, 40.0, 40.0, car.color);
-            match car.direction {
-                Direction::Up => {
+            match car.depart {
+                Depart::Up => {
                     car.y += 1.0;
                 }
-                Direction::Down => {
+                Depart::Down => {
                     car.y -= 1.0;
                 }
-                Direction::Left => {
+                Depart::Left => {
                     car.x += 1.0;
                 }
-                Direction::Right => {
+                Depart::Right => {
                     car.x -= 1.0;
                 }
+            }
+
+            match car.direction {
+                Direction::North => {
+                    if car.x == 355.0 {
+                        car.depart = Depart::Down;
+                    }
+                },
+                Direction::South => {
+                    if car.x == 305.0 {
+                        car.depart = Depart::Up;
+                    }
+                },
+                Direction::East => {
+                    if car.y == 305.0 {
+                        car.depart = Depart::Right;
+                    }
+                },
+                Direction::West => {
+                    if car.y == 355.0 {
+                        car.depart = Depart::Left;
+                    }
+                },
+
             }
 
             
@@ -115,6 +186,17 @@ async fn main() {
             && car.y > -40.0
             && car.y < screen_height()
         });
+
+        for light in &lights {
+            draw_rectangle(light.x, light.y , 10.0 , 20.0, BLACK);
+            if light.is_on {
+                draw_circle(light.x+5.0, light.y+5.0 , 3.0, GREEN);
+                draw_circle(light.x+5.0, light.y+15.0 , 3.0, DARKGRAY);
+            } else {
+                draw_circle(light.x+5.0, light.y+5.0 , 3.0, DARKGRAY);
+                draw_circle(light.x+5.0, light.y+15.0 , 3.0, RED);
+            }
+        }
 
         println!("{:?}",cars);
  
